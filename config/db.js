e625795543c77d2743b86e5cfa55e82278bce90a
Dazuva <159646919+Dazuva@users.noreply.github.com>
2024-12-17 21:27:32 -0600

    Actualizacion

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 async function connectDB() {
   try {
-    await mongoose.connect('mongodb+srv://danielazuvaldo:Dazuva123@clusterdazuva.rqdbi.mongodb.net/productsCRUD');
+    await mongoose.connect('mongodb+srv://danielazuvaldo:Dazuva123@clusterdazuva.rqdbi.mongodb.net/estudiantesCRUD');
     console.log('Conexión exitosa a MongoDB');
   } catch (error) {
     console.error('Error al conectar con MongoDB:', error);
--- routes/rutas.js
+++ routes/rutas.js
@@ -1,10 +1,10 @@
 import routerEstudiante from "./estudianteRouter.js";
 import routerClase from "./claseRouter.js";
 
-// Permite manejar las rutas con el ruteo
+
 function routerApi(app) {
-    app.use("/estudiante", routerEstudiante); // Registra el enrutador de estudiantes
-    app.use("/clase", routerClase); // Registra el enrutador de clases
+    app.use("/estudiante", routerEstudiante);
+    app.use("/clase", routerClase);
 }
 
 export default routerApi;
